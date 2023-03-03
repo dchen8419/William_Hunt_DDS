1404ab055212a6048ddf7a5a5475f703fc7e45a9
David Chen <DavidChen@Davids-iMac.local>
2023-03-03 12:05:15 -0500

    added header with phone number, location and appointment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/navbar/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Header />
+      <Nav />
+      <About />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header-container'>
+            <div>Forest City</div>
+            <div>Request Your Appointment</div>
+            <div>Phone Number</div>
+        </div>
+    )
+}
+
+export default Header
