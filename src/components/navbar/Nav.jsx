909dd90e8b28aa35d819a7b2b49fc35dccc9a52e
David Chen <DavidChen@Davids-iMac.local>
2023-03-06 21:47:39 -0500

    fixed navbar

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import './Nav.css'
 
 function Nav() {
+    const onHome = () => {
+        window.location.href = '#Home';
+    }
+
+    const onAbout = () => {
+        window.location.href = "#About";
+    }
+
+    const onContact = () => {
+        window.location.href = "#Contact";
+    } 
+
     return (
-        <div>Nav</div>
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <a className='navbar-logo' href='#Home' aria-hidden="true">
+                        W.Hunt, DDS Family Dentistry
+                    </a>
+                <ul>
+                    <li>
+                    <a href="#Home" onClick={onHome}>
+                        Home
+                    </a>
+                    </li>
+                    <li>
+                    <a href="#About" onClick={onAbout}>
+                        About
+                    </a>
+                    </li>
+                    <li>
+                    <a href="#Contact" onClick={onContact}>
+                        Contact
+                    </a>
+                    </li>
+                </ul>
+                </div>
+            </nav>
+        </>
     )
 }
 
