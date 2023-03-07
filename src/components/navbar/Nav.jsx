65d262ceaddc3e4b66f4c92fa9142521b4730aba
David Chen <DavidChen@Davids-iMac.local>
2023-03-07 13:43:51 -0500

    fixed some css of navbar

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -19,25 +19,25 @@ function Nav() {
             <nav className='navbar'>
                 <div className='navbar-container'>
                     <a className='navbar-logo' href='#Home' aria-hidden="true">
-                        W.Hunt, DDS Family Dentistry
+                        William Hunt, DDS Family Dentistry
                     </a>
-                <ul>
-                    <li>
-                    <a href="#Home" onClick={onHome}>
-                        Home
-                    </a>
-                    </li>
-                    <li>
-                    <a href="#About" onClick={onAbout}>
-                        About
-                    </a>
-                    </li>
-                    <li>
-                    <a href="#Contact" onClick={onContact}>
-                        Contact
-                    </a>
-                    </li>
-                </ul>
+                    <ul>
+                        <li>
+                            <a href="#Home" onClick={onHome}>
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#About" onClick={onAbout}>
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#Contact" onClick={onContact}>
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
