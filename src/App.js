2418e65e6df470328212f06b7131fe7c7cf7f79b
David Chen <DavidChen@Davids-iMac.local>
2023-03-20 14:57:46 -0400

    added picture, banner and more about

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import About from './components/about/About';
+import Banner from './components/banner/Banner';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Nav from './components/navbar/Nav';
+
 
 function App() {
   return (
     <div>
       <Header />
       <Nav />
+      <Banner />
       <About />
       <Contact />
       <Footer />
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,9 @@ import './Header.css'
 function Header() {
     return (
         <div className='header-container'>
-            <div>Forest City</div>
+            <div>Forest City, NC</div>
             <div>Request Your Appointment</div>
-            <div>(828) 245-3206</div>
+            <div>828-245-3206</div>
         </div>
     )
 }
