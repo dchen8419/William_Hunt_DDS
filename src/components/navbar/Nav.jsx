84c415879367b1a74245fc0096c87964a02094a0
David Chen <DavidChen@Davids-iMac.local>
2023-03-22 14:25:12 -0400

    fixed responsive nav bar

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,4 +1,4 @@
-import {useRef} from 'react'
+import { useRef } from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import '../navbar/Nav.css'
 import icon from '../../assets/dentist_icon.png'
@@ -6,10 +6,13 @@ import icon from '../../assets/dentist_icon.png'
 function Nav() {
     const navRef = useRef();
 
+
+
+
     const showNavbar = () => {
         navRef.current.classList.toggle(
             "responsive_nav"
-            )
+        )
     }
 
 
@@ -19,16 +22,16 @@ function Nav() {
                 <img alt='dentalicon' src={icon} className="dentalicon"/>
             </div>
         <nav ref={navRef}>
-            <a href="#Home" >Home</a>
-            <a href="#About" >About</a>
-            <a href="#Contact" >Contact</a>
-            <button className='navbar-btn nav-close-btn' onClick={showNavbar}>
+            <a href="#Home" onClick={showNavbar}>Home</a>
+            <a href="#About" onClick={showNavbar}>About</a>
+            <a href="#Contact" onClick={showNavbar}>Contact</a>
+            <button className='navbar-btn nav-close-btn' onClick={showNavbar} >
                 <FaTimes />
             </button>
         </nav>
-        <button className='navbar-btn' onClick={showNavbar}>
-            <FaBars />
-        </button>
+            <button className='navbar-btn' onClick={showNavbar} >
+                <FaBars />
+            </button>
     </header>
     )
 }
